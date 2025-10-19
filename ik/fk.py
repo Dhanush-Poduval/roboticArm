13de4d69f3bc4a918bbf8f9238984aa6c4bd4fc8
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-10-19 18:43:15 +0530

    fix:changes fk for 2d values temporary

--- ik/fk.py
+++ ik/fk.py
@@ -2,9 +2,12 @@ import numpy as np
 from dh import dh_transform
 import math
 
-def forward_kinematics(theta1,theta2,theta3,d1,L2,L3):
-    A1=dh_transform(0,math.pi/2,d1,theta1)
-    A2=dh_transform(L2,0,0,theta2)
-    A3=dh_transform(L3,0,0,theta3)
+def forward_kinematics(theta1,theta2,theta3,L1,L2,L3):
+    rad_1=math.radians(theta1)
+    rad_2=math.radians(theta2)
+    rad_3=math.radians(theta3)
+    A1=dh_transform(L1,0,0,rad_1)
+    A2=dh_transform(L2,0,0,rad_2)
+    A3=dh_transform(L3,0,0,rad_3)
     T=A1 @ A2 @ A3
     return T[:3,3]
