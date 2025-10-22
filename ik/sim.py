0df389f8978276505bbbf134050bd1e4fa2dd8a3
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-10-22 22:31:13 +0530

    feat:made the first movement of the robot

--- ik/sim.py
+++ ik/sim.py
@@ -10,6 +10,16 @@ pybullet.loadURDF("plane.urdf")
 
 robot_arm=pybullet.loadURDF('kuka_iiwa/model.urdf',useFixedBase=True)
 
+print(pybullet.getNumJoints(robot_arm))
+
+position,orientation=pybullet.getBasePositionAndOrientation(robot_arm)
+print(position,orientation)
+
+pybullet.getJointInfo(robot_arm,3)
+
+pybullet.setGravity(0,0,-9.81)
+pybullet.setRealTimeSimulation(1)
+pybullet.setJointMotorControlArray(robot_arm,range(4),pybullet.POSITION_CONTROL,targetPositions=[0.2]*4)
 while True:
     pybullet.stepSimulation()
     time.sleep(1./240.)
