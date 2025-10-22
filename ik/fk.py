f94da8957c975292ecaa814aeff393fb469cdeff
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-10-22 17:05:01 +0530

    feat:added real dimensions of our arm

--- ik/fk.py
+++ ik/fk.py
@@ -6,7 +6,8 @@ def forward_kinematics(theta1, theta2, theta3 ,L1 ,L2 ,L3):
     t1 = math.radians(theta1)
     t2 = math.radians(theta2)
     t3 = math.radians(theta3)
-
+    x0=0
+    y0=0
     x1 = L1 * math.cos(t1)
     y1 = L1 * math.sin(t1)
 
@@ -16,4 +17,4 @@ def forward_kinematics(theta1, theta2, theta3 ,L1 ,L2 ,L3):
     x3 = x2 + L3 * math.cos(t1 + t2 + t3)
     y3 = y2 + L3 * math.sin(t1 + t2 + t3)
     
-    return (x1, y1), (x2, y2), (x3, y3)#this is just to check the position of like all the joints of the arm 
+    return (x0,y0),(x1, y1), (x2, y2), (x3, y3)#this is just to check the position of like all the joints of the arm 
--- ik/ik.py
+++ ik/ik.py
@@ -3,20 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 from fk import forward_kinematics
-L1, L2, L3 = 10, 10, 5
+L1, L2, L3 = 70,150,180
 
-def inverse_kinematics(x, y):
-    r = math.sqrt(x**2 + y**2)
-    wx=x-L3*(x/r)
-    wy=y-L3*(y/r)   
-    rw=math.sqrt(wx**2 + wy**2)
-    cos_theta2 = (rw**2 - L1**2 - L2**2) / (2 * L1 * L2)
-    cos_theta2 = max(-1, min(1, cos_theta2))
-    theta2 = math.acos(cos_theta2)
-    theta1 = math.atan2(wy, wx)-math.atan2(L2 * math.sin(theta2), L1 + L2 * math.cos(theta2))
-    theta3 = math.atan2(y - wy, x - wx) - (theta1 + theta2)
-    if r>L1+L2+L3:
+def inverse_kinematics(x, y, phi=0):
+    wx = x - L3 * math.cos(phi)
+    wy = y - L3 * math.sin(phi)
+    rw = math.hypot(wx, wy)
+
+    if rw > (L1 + L2) or rw < abs(L1 - L2):
+        print("Target out of reach")
         return False
+
+    cos_theta2 = (rw**2 - L1**2 - L2**2) / (2 * L1 * L2)
+    cos_theta2 = max(-1, min(1, cos_theta2))  
+    theta2 = math.acos(cos_theta2)  
+  
+    theta1 = math.atan2(wy, wx) - math.atan2(L2 * math.sin(theta2), L1 + L2 * math.cos(theta2))
+    theta3 = phi - (theta1 + theta2)
     return math.degrees(theta1), math.degrees(theta2), math.degrees(theta3)
 def plot(angles):
     theta1,theta2,theta3=[math.radians(i) for i in angles]
@@ -27,8 +30,8 @@ def plot(angles):
     plt.figure()
     plt.plot([x0,x1,x2,x3],[y0,y1,y2,y3],linewidth=3,markersize=8,marker='o')
     plt.plot(x3,y3,'rx',markersize=10)
-    plt.xlim(-30,30)
-    plt.ylim(-5,30)
+    plt.xlim(-250,250)
+    plt.ylim(-25,500)
     plt.grid()
     plt.gca().set_aspect('equal',adjustable='box')
     plt.show()
@@ -43,40 +46,56 @@ def move_arm(start , end , steps=50):
         a3 = a3_arr[i]
         plot((a1, a2, a3))
         time.sleep(0.05)
-def automate(targets,steps=50):
+
+def automate(targets, steps=50):
     plt.ion()
-    fig,ax=plt.subplots()
-    ax.set_xlim(-25,25)
-    ax.set_ylim(-5,25)
+    fig, ax = plt.subplots()
+    ax.set_xlim(-300, 300)
+    ax.set_ylim(-100, 400)
     ax.set_aspect('equal')
     ax.grid(True)
 
+   
+    curr_angles = (0, 0, 0)
+
     for target in targets:
-        x,y=target
-        angles=inverse_kinematics(x,y)
+        x, y = target
+        angles = inverse_kinematics(x, y)
         if not angles:
-            print(f"Taget {target} out of reach")
-        theta1,theta2,theta3=angles
-        print(f"Moving to target {target} lesgo")
-        for i in np.linspace(0,1 ,steps):
-            (x0,y0),(x1,y1),(x2,y2)=forward_kinematics(theta1*i,theta2*i,theta3*i,L1,L2,L3)
+            print(f"Target {target} out of reach")
+            continue
+
+        print(f"Moving to target {target}...")
+
+       
+        theta1_arr = np.linspace(curr_angles[0], angles[0], steps)
+        theta2_arr = np.linspace(curr_angles[1], angles[1], steps)
+        theta3_arr = np.linspace(curr_angles[2], angles[2], steps)
+
+        for t1, t2, t3 in zip(theta1_arr, theta2_arr, theta3_arr):
+            (x0, y0), (x1, y1), (x2, y2), (x3, y3) = forward_kinematics(t1, t2, t3, L1, L2, L3)
             ax.clear()
-            ax.plot([x0,x1,x2],[y0,y1,y2],'o-',linewidth=3,markersize=8)
-            ax.plot(x,y,'rx',markersize=10)
-            ax.set_ylim(-5, 25)
+            ax.plot([x0, x1, x2, x3], [y0, y1, y2, y3], 'o-', linewidth=3, markersize=8)
+            ax.plot(x, y, 'rx', markersize=10)
+            ax.set_xlim(-300, 300)
+            ax.set_ylim(-100, 400)
             ax.set_aspect('equal')
             ax.grid(True)
             plt.draw()
-            plt.pause(0.02)
+            plt.pause(0.5)
+
+        curr_angles = angles  
+
     plt.ioff()
     plt.show()
 
-        
 
+target = [(150, 150), (200, 100), (100, 200)]
 
-target =  [(13,9), (15, 5), (1,6)]
 angle_list=[]
-automate(target)
+automate(target,steps=30)
+
+#This part is for manual checking of fk and ik functions
 '''for targets in target:
     angles=inverse_kinematics(*targets)
     angle_list.append(angles)
@@ -85,11 +104,6 @@ automate(target)
         print(f'Joint Angles :{angles}')
     else:
         print("Target out of reach")
-for i in range(len(angle_list)):
-    start = angle_list[i]
-    end = angle_list[i + 1]
-    if start and end:
-        move_arm(start, end)
 theta1=angle_list[0][0]
 theta2=angle_list[0][1]
 theta3=angle_list[0][2]
