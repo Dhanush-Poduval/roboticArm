fc4fd1bb80d810492aec32be147d75990c77e305
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-10-23 21:19:51 +0530

    feat:converted to 4DOF

--- ik/fk.py
+++ ik/fk.py
@@ -1,11 +1,12 @@
 import numpy as np
 import math
 
-def forward_kinematics(theta1, theta2, theta3 ,L1 ,L2 ,L3):
+def forward_kinematics(theta1, theta2, theta3 ,theta4,L1 ,L2 ,L3,L4):
     
     t1 = math.radians(theta1)
     t2 = math.radians(theta2)
     t3 = math.radians(theta3)
+    t4=math.radians(theta4)
     x0=0
     y0=0
     x1 = L1 * math.cos(t1)
@@ -16,5 +17,8 @@ def forward_kinematics(theta1, theta2, theta3 ,L1 ,L2 ,L3):
 
     x3 = x2 + L3 * math.cos(t1 + t2 + t3)
     y3 = y2 + L3 * math.sin(t1 + t2 + t3)
+
+    x4=x3+L4*math.cos(t1+t2+t3+t4)
+    y4=y3+L4*math.sin(t1+t2+t3+t4)
     
-    return (x0,y0),(x1, y1), (x2, y2), (x3, y3)#this is just to check the position of like all the joints of the arm 
+    return (x0,y0),(x1, y1), (x2, y2), (x3, y3),(x4,y4)#this is just to check the position of like all the joints of the arm 
--- ik/ik.py
+++ ik/ik.py
@@ -3,32 +3,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 from fk import forward_kinematics
-L1, L2, L3 = 70,150,180
+L1, L2, L3 ,L4= 70,150,180,60
 
 def inverse_kinematics(x, y, phi=0):
-    wx = x - L3 * math.cos(phi)
-    wy = y - L3 * math.sin(phi)
+    wx = x - L4 * math.cos(phi)
+    wy = y - L4 * math.sin(phi)
     rw = math.hypot(wx, wy)
-
+    L23=L2+L3
     if rw > (L1 + L2) or rw < abs(L1 - L2):
         print("Target out of reach")
         return False
-
-    cos_theta2 = (rw**2 - L1**2 - L2**2) / (2 * L1 * L2)
+   
+    cos_theta2 = (rw**2 - L1**2 - L23**2) / (2 * L1 * L23)
     cos_theta2 = max(-1, min(1, cos_theta2))  
     theta2 = math.acos(cos_theta2)  
-  
+    theta3 = 0
     theta1 = math.atan2(wy, wx) - math.atan2(L2 * math.sin(theta2), L1 + L2 * math.cos(theta2))
-    theta3 = phi - (theta1 + theta2)
-    return math.degrees(theta1), math.degrees(theta2), math.degrees(theta3)
+    theta4 = phi - (theta1 + theta2+theta3)
+    return math.degrees(theta1), math.degrees(theta2), math.degrees(theta3),math.degrees(theta4)
 def plot(angles):
     theta1,theta2,theta3=[math.radians(i) for i in angles]
     x0,y0=0,0
     x1,y1=L1*math.cos(theta1),L1*math.sin(theta1)
     x2,y2=x1+L2*math.cos(theta1+theta2),y1+L2*math.sin(theta1+theta2)
     x3,y3=x2+L3*math.cos(theta1+theta2+theta3),y2+L3*math.sin(theta1+theta2+theta3)
+    x4,y4=x3+L4*math.cos(theta1+theta2+theta3),y3+L4*math.sin(theta1+theta2+theta3)
     plt.figure()
-    plt.plot([x0,x1,x2,x3],[y0,y1,y2,y3],linewidth=3,markersize=8,marker='o')
+    plt.plot([x0,x1,x2,x3,x4],[y0,y1,y2,y3,y4],linewidth=3,markersize=8,marker='o')
     plt.plot(x3,y3,'rx',markersize=10)
     plt.xlim(-250,250)
     plt.ylim(-25,500)
@@ -40,11 +41,14 @@ def move_arm(start , end , steps=50):
     a1_arr=np.linspace(start[0],end[0],steps)
     a2_arr = np.linspace(start[1], end[1], steps)
     a3_arr= np.linspace(start[2],end[2], steps)
+    a4_arr = np.linspace(start[3], end[3], steps)
+
     for i in range(steps+1):
         a1 = a1_arr[i]
         a2 = a2_arr[i]
         a3 = a3_arr[i]
-        plot((a1, a2, a3))
+        a4=a4_arr[i]
+        plot((a1, a2, a3,a4))
         time.sleep(0.05)
 
 def automate(targets, steps=50):
@@ -56,7 +60,7 @@ def automate(targets, steps=50):
     ax.grid(True)
 
    
-    curr_angles = (0, 0, 0)
+    curr_angles = (0, 0, 0,0)
 
     for target in targets:
         x, y = target
@@ -71,18 +75,18 @@ def automate(targets, steps=50):
         theta1_arr = np.linspace(curr_angles[0], angles[0], steps)
         theta2_arr = np.linspace(curr_angles[1], angles[1], steps)
         theta3_arr = np.linspace(curr_angles[2], angles[2], steps)
-
-        for t1, t2, t3 in zip(theta1_arr, theta2_arr, theta3_arr):
-            (x0, y0), (x1, y1), (x2, y2), (x3, y3) = forward_kinematics(t1, t2, t3, L1, L2, L3)
+        theta4_arr = np.linspace(curr_angles[3], angles[3], steps)
+        for t1, t2, t3 , t4 in zip(theta1_arr, theta2_arr, theta3_arr,theta4_arr):
+            (x0, y0), (x1, y1), (x2, y2), (x3, y3),(x4,y4) = forward_kinematics(t1, t2, t3,t4, L1, L2, L3,L4)
             ax.clear()
-            ax.plot([x0, x1, x2, x3], [y0, y1, y2, y3], 'o-', linewidth=3, markersize=8)
+            ax.plot([x0, x1, x2, x3 , x4], [y0, y1, y2, y3 , y4], 'o-', linewidth=3, markersize=8)
             ax.plot(x, y, 'rx', markersize=10)
             ax.set_xlim(-300, 300)
             ax.set_ylim(-100, 400)
             ax.set_aspect('equal')
             ax.grid(True)
             plt.draw()
-            plt.pause(0.5)
+            plt.pause(0.02)
 
         curr_angles = angles  
 
