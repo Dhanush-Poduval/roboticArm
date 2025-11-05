61aff23153cbc08c187136000dcd8da6f973bd61
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-11-05 23:32:34 +0530

    feat:made the code to get each point of the container nice

--- main/arm_movement.py
+++ main/arm_movement.py
@@ -1,7 +1,14 @@
 from .check import detected_aruco_positions,container_positions
 from ..ik.motor_control import motor_movement
+from ..ik.inverse_k import is_valid
 #this will be the ik function for easy access
-def inverse_kinematics():
+J1,J2,J3,J4=20,130,150,60
+containers=container_positions.values()
+ik_target_pos=list(containers)
+print("Containers seen are ")
+for i , c in enumerate(ik_target_pos):
+    print(f"x={c[0]},y={c[1]:.2f},z={c[3]:.2f}")
+def inverse_kinematics(J1,J2,J3,J4,):
     pass
 
 #this will be the clearence position movement
