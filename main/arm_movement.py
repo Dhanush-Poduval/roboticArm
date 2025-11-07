06ceb168220cce6624d0e594a391c8b7edcf82b8
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-11-07 18:16:30 +0530

    feat:added it to the arm movement part

--- main/arm_movement.py
+++ main/arm_movement.py
@@ -14,8 +14,14 @@ for i , c in enumerate(ik_target_pos):
     print(f"x={c[0]},y={c[1]:.2f},z={c[2]:.2f}")
 
 port='/dev/ttyUSB0'
-
-
+baud_rate=115200
+try:
+        arduino=serial.Serial(port,baud_rate,timeout=1)
+        time.sleep(2)
+        print("Connected to port ")
+except serial.SerialException as e:
+        print(f"Errot :{e}")
+        exit()
 
 def inverse_kinematics(J1,J2,J3,J4,x,y,z):
     z_final=z-J4-J1
@@ -64,45 +70,43 @@ def clearence_position(x , y, z):
     clearence_pos=[x-0.5,y-0.5,z-0.5]#keeping dynamic for now will change to static if issue
     print(f"Clearence position is : {clearence_pos[0],clearence_pos[1],clearence_pos[2]}")
     inverse_kinematics(J1,J2,J3,J4,clearence_pos[0],clearence_pos[1],clearence_pos[2])
-def motor_movement(theta1 , theta2 , theta3 , theta4):
-    baud_rate=115200
+def motor_movement(angle1 , angle2 , angle3 , angle4):
 
+    
     try:
-        arduino=serial.Serial(port,baud_rate,timeout=1)
-        time.sleep(2)
-        print("Connected to port ")
-    except serial.SerialException as e:
-        print(f"Errot :{e}")
-        exit()
+            command1=str(angle1) +'\n'
+            arduino.write(command1.encode('utf-8'))
+            command2=str(angle2)+'\n'
+            arduino.write(command2.encode('utf-8'))
+            command3=str(angle3)+'\n'
+            arduino.write(command3.encode('utf-8'))
+            command4=str(angle4)+'\n'
+            arduino.write(command4.encode('utf-8'))
+            print(f"All the angles sent {angle1,angle2,angle3,angle4}")
 
-    def servo_angle(angle):
-        try:
-            command=str(angle) +'\n'
-            arduino.write(command.encode('utf-8'))
-            print(f'Angle {angle}')
-        except Exception as e:
+    except Exception as e:
             print(f"Error :  {e}")
-    try :
-        while True :
-            angle_input=input("Enter desired angle")
-            if angle_input.lower()=='q':
-                break
-            try:
-                angle=int(angle_input)
-                if 0<=angle<=180:
-                    servo_angle(angle)
-                else:
-                    print('Wrong angle')
-            except ValueError:
-                print("Invalid input. Please enter an integer.")
+    # try :
+    #     while True :
+    #         angle_input=input("Enter desired angle")
+    #         if angle_input.lower()=='q':
+    #             break
+    #         try:
+    #             angle=int(angle_input)
+    #             if 0<=angle<=180:
+    #                 servo_angle(angle)
+    #             else:
+    #                 print('Wrong angle')
+    #         except ValueError:
+    #             print("Invalid input. Please enter an integer.")
 
     except KeyboardInterrupt:
-        print("Program interrupted")
+            print("Program interrupted")
 
     finally:
-        if arduino.is_open:
-            arduino.close()
-            print("Serial port closed")
+            if arduino.is_open:
+                arduino.close()
+                print("Serial port closed")
 
 def retry():
     pass
