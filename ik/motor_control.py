d813488dbe986b69b9e6dcc1cccbc287c18ac35c
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-11-07 18:12:20 +0530

    feat:updated the motor control code for more than one angle all 4 angles

--- ik/motor_control.py
+++ ik/motor_control.py
@@ -13,32 +13,39 @@ except serial.SerialException as e:
     print(f"Errot :{e}")
     exit()
 
-def servo_angle(angle):
+def servo_angle(angle1,angle2 ,angle3 ,angle4):
     try:
-        command=str(angle) +'\n'
-        arduino.write(command.encode('utf-8'))
-        print(f'Angle {angle}')
+        command1=str(angle1) +'\n'
+        arduino.write(command1.encode('utf-8'))
+        command2=str(angle2)+'\n'
+        arduino.write(command2.encode('utf-8'))
+        command3=str(angle3)+'\n'
+        arduino.write(command3.encode('utf-8'))
+        command4=str(angle4)+'\n'
+        arduino.write(command4.encode('utf-8'))
+        print(f"All the angles sent {angle1,angle2,angle3,angle4}")
+
     except Exception as e:
         print(f"Error :  {e}")
-try :
-    while True :
-        angle_input=input("Enter desired angle")
-        if angle_input.lower()=='q':
-            break
-        try:
-            angle=int(angle_input)
-            if 0<=angle<=180:
-                servo_angle(angle)
-            else:
-                print('Wrong angle')
-        except ValueError:
-            print("Invalid input. Please enter an integer.")
+# try :
+#     while True :
+#         angle_input=input("Enter desired angle")
+#         if angle_input.lower()=='q':
+#             break
+#         try:
+#             angle=int(angle_input)
+#             if 0<=angle<=180:
+#                 servo_angle(angle)
+#             else:
+#                 print('Wrong angle')
+#         except ValueError:
+#             print("Invalid input. Please enter an integer.")
 
-except KeyboardInterrupt:
-    print("Program interrupted")
+    except KeyboardInterrupt:
+        print("Program interrupted")
 
-finally:
-    if arduino.is_open:
-        arduino.close()
-        print("Serial port closed")
-        
+    finally:
+        if arduino.is_open:
+            arduino.close()
+            print("Serial port closed")
+            
